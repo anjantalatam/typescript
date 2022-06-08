75ec9f99c591b0d9a07291d00f8976b2dd121cf1
Anjan Talatam <tspa2117@gmail.com>
2022-06-08 20:08:29 +0530

    Passing different type of Props

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -2,18 +2,35 @@ import React from "react";
 
 interface ChildProps {
   color: string;
+  onClick: () => void;
+  children: string;
 }
 
-export function Child({ color }: ChildProps): JSX.Element | null {
-  return <div>{color}</div>;
+export function Child({ color, onClick }: ChildProps): JSX.Element | null {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 }
 
 // Child1 cannot access React Component Properties.
-export const Child1 = ({ color }: ChildProps) => {
-  return <div>{color}</div>;
+export const Child1 = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
 
 //Child2 Can
-export const Child2: React.FC<ChildProps> = ({ color }) => {
-  return <div>{color}</div>;
+export const Child2: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Child } from "./Child";
 
 function Parent() {
-  return <Child color="red" />;
+  return (
+    <Child color="red" onClick={() => console.log("Clicked")}>
+      ...
+    </Child>
+  );
 }
 
 export default Parent;
