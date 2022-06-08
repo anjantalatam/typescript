88abc923b6286a8e4351393c2594d06179a141a2
Anjan Talatam <tspa2117@gmail.com>
2022-06-08 19:42:05 +0530

    Use React methods on component by extending it's type

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -4,6 +4,16 @@ interface ChildProps {
   color: string;
 }
 
-export function Child({ color }: ChildProps) {
+export function Child({ color }: ChildProps): JSX.Element | null {
   return <div>{color}</div>;
 }
+
+// Child1 cannot access React Component Properties.
+export const Child1 = ({ color }: ChildProps) => {
+  return <div>{color}</div>;
+};
+
+//Child2 Can
+export const Child2: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
