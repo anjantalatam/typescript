50cffdcc5c586398d97b3153581cd1803c87df95
Anjan Talatam <tspa2117@gmail.com>
2022-07-05 18:28:11 +0530

    18 - Typescript with class components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import UserSearch from "./classes/UserSearch";
 import EventComponent from "./events/EventComponent";
 
 const root = ReactDOM.createRoot(
@@ -10,6 +11,13 @@ const App = () => (
   <div>
     <h1>Hi there!</h1>
     <EventComponent />
+    <UserSearch
+      users={[
+        { name: "anan", age: 10 },
+        { name: "billy", age: 20 },
+        { name: "charlie", age: 30 },
+      ]}
+    />
   </div>
 );
 
