0ba15034f40fd9ab6b07d0f784f1f3899258c4a2
Anjan Talatam <tspa2117@gmail.com>
2022-07-08 22:30:35 +0530

    32 - Adding Request Logic

--- redux-ts/src/state/action-creator/index.ts
+++ redux-ts/src/state/action-creator/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ActionType } from "../action-type";
 
 export const searchRepositories = (term: string) => {
@@ -6,6 +7,21 @@ export const searchRepositories = (term: string) => {
       type: ActionType.SEARCH_REPOSITORIES,
     });
     try {
+      const { data } = await axios.get(
+        "http://registry.npmjs.com/-/v1/search",
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.objects.map((res: any) => res.package.name);
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (err: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
