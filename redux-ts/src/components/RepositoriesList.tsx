5f4ce798081b730388df2669b23c40a8e890bfc0
Anjan Talatam <tspa2117@gmail.com>
2022-07-09 18:26:12 +0530

    37 - Reminder on Event Types

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
